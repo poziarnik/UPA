8e37e98e9ca1f4a8aa9041723713efec49739a46
Ades551 <ades6800@gmail.com>
2023-09-23 16:55:32 +0200

    added mongodb compose

---
+++ Makefile
@@ -0,0 +1,13 @@
+ACTIVATE=. venv/bin/activate
+
+define run
+	podman-compose --file $(1)/$(1).yml up -d
+	$(ACTIVATE) && python3 $(1)/run.py
+endef
+
+venv:
+	python3 -m venv venv
+	$(ACTIVATE) && pip3 install -r requirements.txt
+
+run-mongo:
+	$(call run,"mongo")
---
+++ mongo/run.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+HOSTNAME = "localhost"
+PORT = 8081
+USERNAME = "root"
+PASSWORD = "root"
+
+CONNECTION_STRING = f"mongodb://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}"
+
+client = MongoClient(HOSTNAME, PORT, username=USERNAME, password=PASSWORD)
+print(client.list_database_names())
